e878776a8cc9563d4784c29edbc05a002cbaa453
Marcel <edmondmarcellinuskamji@gmail.com>
2026-08-18 21:42:13 +0100

    non circular queue with shift

---
+++ lib/queue/non_circular_queue_with_shift.dart
@@ -0,0 +1,64 @@
+import 'package:dsa/arrays/dynamic_array.dart';
+
+class Queue<T> {
+  final DynamicArray<T> _data;
+
+  Queue() : _data = DynamicArray<T>();
+
+  /*
+  Enqueue
+  FUNCTION enqueue(newValue)
+  data.add(newValue)
+   */
+
+  void enqueue(T newValue) {
+    _data.add(newValue);
+  }
+
+  /*
+  Dequeue
+  FUNCTION dequeue()
+  IF isEmpty()
+    THROW 'Queue underflow;
+    top = data.get(0)
+    data.remove(0)
+    RETURN Top
+   */
+  T dequeue() {
+    if (isEmpty) {
+      throw StateError('Queue underfow');
+    }
+    final value = _data.remove(0);
+    return value;
+  }
+
+  /*
+  Peek
+  FUNCTION peek
+  IF isEmpty()
+    THROW 'Queue is Empty'
+    RETURN data.get(0)
+   */
+
+  T peek() {
+    if (isEmpty) {
+      throw StateError('Queue is Empty');
+    }
+    return _data.get(0);
+  }
+
+  /*
+  isEmpty
+  FUNCTION isEmpty()
+    RETURN data.isEmpty()
+   */
+  bool get isEmpty => _data.size == 0;
+
+  /*
+size
+ FUNCTION size()
+  RETURN data.size()
+   */
+
+  int get size => _data.size;
+}
