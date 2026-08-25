31807dc97486646d054dbdc8754da4dcd960f503
Marcel <edmondmarcellinuskamji@gmail.com>
2026-08-25 16:07:57 +0100

    singly_linked_list_without_tail

---
+++ lib/linked_list/singly_linked_list_without_tail.dart
@@ -0,0 +1,49 @@
+//lib/linked_list/singly_linked_list_without_tail.dart
+
+//CREATE NODE
+class Node<T> {
+  T value;
+  Node<T>? next;
+
+  Node(this.value, [this.next]);
+}
+
+//CREATE SINGLY LINKED LIST
+class SinglyLinkedListWithoutTail<T> {
+  Node<T>? head;
+
+  /*
+  PREPEND 
+  CREATE a newNode
+  newNode = Node(data)
+  newNode.next = head
+  head = newNode
+   */
+
+  void prepend(T data) {
+    final newNode = Node<T>(data);
+    newNode.next = head;
+    head = newNode;
+  }
+
+  void append(T data) {
+    final newNode = Node<T>(data);
+    if (head == null) {
+      head = newNode;
+      return;
+    }
+    Node<T> current = head!;
+    while (current.next != null) {
+      current = current.next!;
+    }
+    current.next = newNode;
+  }
+
+  void printList() {
+    Node<T>? current = head;
+    while (current != null) {
+      print(current.value);
+      current = current.next;
+    }
+  }
+}
