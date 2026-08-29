2b106e9d9e7ba197e9f35b8e7ee63cdf8220a617
Marcel <edmondmarcellinuskamji@gmail.com>
2026-08-29 15:20:06 +0100

    inked_list singly linked list with taiL

---
+++ lib/linked_list/singly_linked_list_with_tail.dart
@@ -0,0 +1,79 @@
+//lib/linked_list/singly_linked_list_with_tail.dart
+//CREATE NODE
+class Node<T> {
+  T value;
+  Node<T>? next;
+
+  Node(this.value, [this.next]);
+}
+
+class SinglyListListWithTail<T> {
+  Node<T>? head;
+  Node<T>? tail;
+
+  /*
+append
+ALGORITHM qppend(data):
+CREATE newNode with data
+    IF list is empty
+        head = newNode;
+        tail = newNode;
+    ELSE 
+        tail.next = newNode
+        tail = newNode
+    END IF
+END ALGORITHM
+*/
+
+  void append(T data) {
+    final newNode = Node<T>(data);
+    if (head == null) {
+      head = newNode;
+      tail = newNode;
+    } else {
+      tail!.next = newNode;
+      tail = newNode;
+    }
+  }
+
+  /*
+prepend
+CREATE newNode with data
+    IF list is empty
+        head = newNode;
+        tail = newNode;
+    ELSE 
+        newNode.next = head
+        head = newNode
+    END IF
+END ALGORITHM
+
+*/
+  void prepend(T data) {
+    final newNode = Node<T>(data);
+    if (head == null) {
+      head = newNode;
+      tail = newNode;
+    } else {
+      newNode.next = head;
+      head = newNode;
+    }
+  }
+
+  /*
+print
+ALGORITHM printList:
+CREATE current
+    WHILE current is not null
+        PRINT current value
+        ADVANCE
+ */
+
+  void printList() {
+    Node<T>? current = head;
+    while (current != null) {
+      print(current.value);
+      current = current.next;
+    }
+  }
+}
