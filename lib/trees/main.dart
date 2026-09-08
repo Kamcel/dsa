7eb7035b5e4521f4739626c4830db5f49582e61a
Marcel <edmondmarcellinuskamji@gmail.com>
2026-09-08 21:57:11 +0100

    final complete

--- lib/trees/main.dart
+++ lib/trees/main.dart
@@ -1,12 +1,15 @@
 import 'package:dsa/trees/tree_node.dart';
 
 void main() {
-  TreeNode<int> node5 = TreeNode(value: 5, left: TreeNode(value: 8));
-  TreeNode<int> node15 = TreeNode(value: 15);
+  TreeNode<int> node5 = TreeNode(value: 5, right: TreeNode(value: 8));
+  TreeNode<int> node15 = TreeNode(value: 20);
   TreeNode<int> root = TreeNode(value: 10, left: node5, right: node15);
 
   print('Root value: ${root.value}');
   print('Left value: ${node5.value}');
   print('Right value: ${node15.value}');
-  print('Left node of 5: ${node5.left?.value}');
+  print('Right node of 5: ${node5.right?.value}');
+  print(
+    'From root to node 8: ${root.value} -> ${root.left?.value} -> ${root.left?.right?.value}',
+  );
 }
